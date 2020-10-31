5246e535ae93a7887384e7d03981df7c4ad89dac
Alex Morozov <igroman-x@list.ru>
2020-10-31 15:20:47 +0300

    testsAdded

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,39 +7,6 @@ fun main() {
     val cardOrAccountType: String
     val transferAmount: Long
 
-    fun countCommission(currentMonthAmount: Long, cardOrAccountType: String, transferAmount: Long): Long {
-        val comission: Long
-        if(cardOrAccountType == "MasterCard" || cardOrAccountType == "Maestro") {
-            return if (currentMonthAmount < 75_000_00) {
-                0
-            } else {
-                comission = transferAmount / 1_000 * 6 + 20_00
-                comission
-            }
-        }
-        else if (cardOrAccountType == "Visa" || cardOrAccountType == "Мир") {
-            return if (transferAmount < 4_666_67) {
-                35_00
-            } else {
-                comission = transferAmount / 10_000 * 75
-                comission
-            }
-        }
-        else {
-            return 0
-        }
-    }
-
-    fun checkLimitsThenOutput(currentMonthAmount: Long, cardOrAccountType: String, transferAmount: Long): String {
-        return if (((cardOrAccountType == "Visa" || cardOrAccountType == "MasterCard" || cardOrAccountType == "Maestro"
-            || cardOrAccountType == "Мир") && currentMonthAmount <= 600_000_00 && transferAmount <= 150_000_00)
-            || (cardOrAccountType == "Счет VK Pay" && currentMonthAmount <= 40_000_00 && transferAmount <= 15_000_00)) {
-            "Комиссия составит ${countCommission(currentMonthAmount, cardOrAccountType, transferAmount)} копеек"
-        } else {
-            "Извините, но вы не укладываетесь в лимиты :("
-        }
-    }
-
     println("Введите тип карты/счета (Visa / MasterCard / Maestro / Мир / Счет VK Pay)")
     cardOrAccountType = scanner.nextLine()
     println("На какую сумму вы уже совершали переводы в этом месяце?")
@@ -47,4 +14,36 @@ fun main() {
     println("Ввведите сумму перевода в копейках")
     transferAmount = scanner.nextLong()
     println(checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount))
+}
+
+fun countCommission(currentMonthAmount: Long, cardOrAccountType: String, transferAmount: Long): Long {
+    val comission: Long
+    if (cardOrAccountType == "MasterCard" || cardOrAccountType == "Maestro") {
+        return if (currentMonthAmount < 75_000_00) {
+            0
+        } else {
+            comission = transferAmount / 1_000 * 6 + 20_00
+            comission
+        }
+    } else if (cardOrAccountType == "Visa" || cardOrAccountType == "Мир") {
+        return if (transferAmount < 4_666_67) {
+            35_00
+        } else {
+            comission = (transferAmount / 10_000 + 1) * 75
+            comission
+        }
+    } else {
+        return 0
+    }
+}
+
+fun checkLimitsThenOutput(currentMonthAmount: Long, cardOrAccountType: String, transferAmount: Long): String {
+    return if (((cardOrAccountType == "Visa" || cardOrAccountType == "MasterCard" || cardOrAccountType == "Maestro"
+                || cardOrAccountType == "Мир") && currentMonthAmount <= 600_000_00 && transferAmount <= 150_000_00)
+        || (cardOrAccountType == "Счет VK Pay" && currentMonthAmount <= 40_000_00 && transferAmount <= 15_000_00)
+    ) {
+        "Комиссия составит ${countCommission(currentMonthAmount, cardOrAccountType, transferAmount)} копеек"
+    } else {
+        "Извините, но вы не укладываетесь в лимиты :("
+    }
 }
---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,197 @@
+package ru.netology
+
+import countCommission
+import checkLimitsThenOutput
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun countCommission_MasterCardAndMinCurrentMonthAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "MasterCard"
+        val transferAmount: Long = 100_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun countCommission_MasterCardAndMaxCurrentMonthAmount() {
+        val currentMonthAmount: Long = 75_000_00
+        val cardOrAccountType = "MasterCard"
+        val transferAmount: Long = 100_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(2060, result)
+    }
+
+    @Test
+    fun countCommission_MaestroAndMinCurrentMonthAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Maestro"
+        val transferAmount: Long = 100_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun countCommission_MaestroAndMaxCurrentMonthAmount() {
+        val currentMonthAmount: Long = 75_000_00
+        val cardOrAccountType = "Maestro"
+        val transferAmount: Long = 100_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(2060, result)
+    }
+
+    @Test
+    fun countCommission_VisaAndMinTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 1_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun countCommission_VisaAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 4_666_67
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(3525, result)
+    }
+
+    @Test
+    fun countCommission_MirAndMinTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Мир"
+        val transferAmount: Long = 1_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun countCommission_MirAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Мир"
+        val transferAmount: Long = 4_666_67
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(3525, result)
+    }
+
+    @Test
+    fun countCommission_VKPay() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Счет VK Pay"
+        val transferAmount: Long = 100_00
+
+        val result = countCommission(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VisaAndMinCurrentMonthAmountAndMinTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 1_00
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Комиссия составит 3500 копеек", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VisaAndMaxCurrentMonthAmountAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 600_000_01
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 150_000_01
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VisaAndMinCurrentMonthAmountAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 150_000_01
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VisaAndMaxCurrentMonthAmountAndMinTransferAmount() {
+        val currentMonthAmount: Long = 600_000_01
+        val cardOrAccountType = "Visa"
+        val transferAmount: Long = 1_00
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VKPayMinCurrentMonthAmountAndMinTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Счет VK Pay"
+        val transferAmount: Long = 1_00
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Комиссия составит 0 копеек", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VKPayMaxCurrentMonthAmountAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 40_000_01
+        val cardOrAccountType = "Счет VK Pay"
+        val transferAmount: Long = 15_000_01
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VKPayMinCurrentMonthAmountAndMaxTransferAmount() {
+        val currentMonthAmount: Long = 0
+        val cardOrAccountType = "Счет VK Pay"
+        val transferAmount: Long = 15_000_01
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+
+    @Test
+    fun checkLimitsThenOutput_VKPayMaxCurrentMonthAmountAndMinTransferAmount() {
+        val currentMonthAmount: Long = 40_000_01
+        val cardOrAccountType = "Счет VK Pay"
+        val transferAmount: Long = 1_00
+
+        val result = checkLimitsThenOutput(currentMonthAmount, cardOrAccountType, transferAmount)
+
+        assertEquals("Извините, но вы не укладываетесь в лимиты :(", result)
+    }
+}
